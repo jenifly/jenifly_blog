e69a0a05f7d147fdc9aa77bbe5b57885767c357a
Jenifly <1439916120@qq.com>
2019-04-29 04:22:24 +0800

    Update config.py

--- server/config.py
+++ server/config.py
@@ -2,17 +2,17 @@ import os
 
 # Application
 settings = {
-  'cookie_secret': 'Qiyp+qz+Eeir9uDVXmfHz0Isqfus/hHos7Dg1V5nx88=',
+  'cookie_secret': '**********************************',
   'xsrf_cookies': True,
   'debug': True,
 }
 
 # mysql
 mysql_options = dict(
-  host='127.0.0.1',
-  database='jenifly_blog',
-  user='root',
-  password='jenifly0.1',
+  host='localhost',
+  database='*************',
+  user='*****',
+  password='*****',
   charset="utf8",
   time_zone='+8:00'
 )
@@ -25,4 +25,4 @@ log_level = 'debug'
 res_path = os.path.join(os.path.dirname(__file__), 'res/')
 
 # key
-passwd_hash_key = 'dFeaFw3JR3Cp4gBkL9Tmo62rY+3mx06zpx3NlVAapbQ='
+passwd_hash_key = '********************************'
